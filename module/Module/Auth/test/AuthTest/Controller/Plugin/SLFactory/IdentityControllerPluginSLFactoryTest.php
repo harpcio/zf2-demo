9c72dc17be2e6571d72e543458b33fe996aea5a5
Rafal Ksiazek <harpcio@gmail.com>
2014-12-20 16:25:32 +0100

    Update License to MIT, update header in all files

--- module/Module/Auth/test/AuthTest/Controller/Plugin/SLFactory/IdentityControllerPluginSLFactoryTest.php
+++ module/Module/Auth/test/AuthTest/Controller/Plugin/SLFactory/IdentityControllerPluginSLFactoryTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file is part of Zf2-demo package
+ *
+ * @author Rafal Ksiazek <harpcio@gmail.com>
+ * @copyright Rafal Ksiazek F.H.U. Studioars
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Module\AuthTest\Controller\Plugin\SLFactory;
 
